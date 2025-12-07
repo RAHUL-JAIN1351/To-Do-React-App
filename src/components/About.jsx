10a06059e3b3229905c3fd6793fd0e2cb4670100
RAHUL-JAIN1351 <rcorrect3@gmail.com>
2025-12-07 11:38:34 +0530

    React To Do App Mod

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='flex w-full justify-evenly border-2 border-blue-400'>
+        <div className='bg-amber-500 h-[100px] w-20'>
+            Box 1
+        </div>
+
+        <div className='bg-amber-500'>
+            Box2
+        </div>
+    </div>
+
+  )
+}
+
+export default About
